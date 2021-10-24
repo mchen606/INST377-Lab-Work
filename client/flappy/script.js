16f779f377ff7bb455cb4e8cb761152885de68bd
mchen606 <michaelc6060@gmail.com>
2021-10-24 12:29:42 -0400

    Update script.js

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -1,15 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded' , () => {
     const bird = document.querySelector('.bird')
     const gameDisplay = document.querySelector('.game-container')
     const ground = document.querySelector('.ground')
 
     let birdLeft = 220
     let birdBottom = 100
+    let gravity = 2
 
 
     function startGame() {
         bird.style.bottom = birdBottom + 'px'
+        bird.style.left = birdLeft + 'px'
     }
+    let timerID = setInterval(startGame, 20)
     
-    <div
 })
